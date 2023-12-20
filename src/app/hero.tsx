4eb59a52fb2894992495cf2142ca15a482c9ccc9
Gary <blimey85@gmail.com>
2023-12-20 12:13:02 -0600

    Initial commit.

---
+++ src/app/hero.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { IconButton, Button, Avatar, Typography } from "@material-tailwind/react";
+//import { PlayIcon } from "@heroicons/react/24/solid";
+
+function Hero() {
+  return (
+    <div className="relative min-h-screen w-full bg-[url('/image/event.jpeg')] bg-cover bg-no-repeat">
+    <div className="absolute inset-0 h-full w-full bg-gray-900/60" />
+    <div className="grid min-h-screen px-8">
+      <div className="container relative z-10 my-auto mx-auto grid place-items-center text-center">
+   
+          <img
+      className="rounded-3xl object-cover object-center"
+      src="/image/oswald.png"
+      alt="Oswald Corbett Portrait"
+    />
+        <Typography variant="h3" color="white" className="mb-2">
+          Oswald Corbett<br />Upcoming Indie Author
+        </Typography>
+        <Typography variant="h1" color="white" className="lg:max-w-3xl">
+          Website Coming Soon!
+        </Typography>
+        
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default Hero;
---
+++ src/components/navbar.tsx
@@ -0,0 +1,121 @@
+import React from "react";
+import {
+  Navbar as MTNavbar,
+  Collapse,
+  Button,
+  IconButton,
+  Typography,
+} from "@material-tailwind/react";
+import {
+  RectangleStackIcon,
+  UserCircleIcon,
+  CommandLineIcon,
+  Squares2X2Icon,
+  XMarkIcon,
+  Bars3Icon,
+} from "@heroicons/react/24/solid";
+
+interface NavItemProps {
+  children: React.ReactNode;
+  href?: string;
+}
+
+function NavItem({ children, href }: NavItemProps) {
+  return (
+    <li>
+      <Typography
+        as="a"
+        href={href || "#"}
+        target={href ? "_blank" : "_self"}
+        variant="paragraph"
+        className="flex items-center gap-2 font-medium"
+      >
+        {children}
+      </Typography>
+    </li>
+  );
+}
+
+const NAV_MENU = [
+  {
+    name: "Page",
+    icon: RectangleStackIcon,
+  },
+  {
+    name: "Account",
+    icon: UserCircleIcon,
+  },
+  {
+    name: "Docs",
+    icon: CommandLineIcon,
+    href: "https://www.material-tailwind.com/docs/react/installation",
+  },
+];
+
+export function Navbar() {
+  const [open, setOpen] = React.useState(false);
+  const [isScrolling, setIsScrolling] = React.useState(false);
+
+  const handleOpen = () => setOpen((cur) => !cur);
+
+  React.useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpen(false)
+    );
+  }, []);
+
+  React.useEffect(() => {
+    function handleScroll() {
+      if (window.scrollY > 0) {
+        setIsScrolling(true);
+      } else {
+        setIsScrolling(false);
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <MTNavbar
+      shadow={false}
+      fullWidth
+      blurred={false}
+      color={isScrolling ? "white" : "transparent"}
+      className="fixed top-0 z-50 border-0"
+    >
+      <div className="container mx-auto flex place-content-center">
+        <Typography
+          color={isScrolling ? "blue-gray" : "white"}
+          className="text-lg font-bold"
+        >
+          Oswald Corbett - Indie Author
+        </Typography>
+        
+      </div>
+      {/* <Collapse open={open}>
+        <div className="container mx-auto mt-4 rounded-lg bg-white px-6 py-5">
+          <ul className="flex flex-col gap-4 text-gray-900">
+            {NAV_MENU.map(({ name, icon: Icon, href }) => (
+              <NavItem key={name} href={href}>
+                <Icon className="h-5 w-5" />
+                {name}
+              </NavItem>
+            ))}
+          </ul>
+          <div className="mt-6 flex items-center gap-4">
+            <Button variant="text">Log in</Button>
+            <a href="https://www.materila-tailwind.com/blocks" target="_blank">
+              <Button color="gray">blocks</Button>
+            </a>
+          </div>
+        </div>
+      </Collapse> */}
+    </MTNavbar>
+  );
+}
+
+export default Navbar;
