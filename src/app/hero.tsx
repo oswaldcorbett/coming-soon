a1b3490fdc214ade32b8fba09858776814287e6d
Oswald <github@oswaldcorbett.com>
2023-12-20 15:34:44 -0500

    Removed coded added to attempt to fix issue.

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -9,16 +9,15 @@ function Hero() {
     <div className="absolute inset-0 h-full w-full bg-gray-900/60" />
     <div className="grid min-h-screen px-8">
       <div className="container relative z-10 my-auto mx-auto grid place-items-center text-center">
-   
           <img
       className="rounded-3xl object-cover object-center"
       src="/image/oswald.png"
       alt="Oswald Corbett Portrait"
     />
-        <Typography variant="h3" color="white" className="mb-2" children={undefined} placeholder={undefined}>
+        <Typography variant="h3" color="white" className="mb-2">
           Oswald Corbett<br />Upcoming Indie Author
         </Typography>
-        <Typography variant="h1" color="white" className="lg:max-w-3xl" children={undefined} placeholder={undefined}>
+        <Typography variant="h1" color="white" className="lg:max-w-3xl">
           Website Coming Soon!
         </Typography>
         
