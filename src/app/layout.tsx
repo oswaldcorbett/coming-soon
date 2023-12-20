d8b77a46dd326a64948001d848cd26ab7dd7c97a
Oswald <github@oswaldcorbett.com>
2023-12-20 15:47:09 -0500

    Updated metadata.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,9 +11,9 @@ const roboto = Roboto({
 });
 
 export const metadata: Metadata = {
-  title: "NextJS Tailwind Event Landing Page",
+  title: "Indie Author Oswald Corbett",
   description:
-    "Introducing Tailwind Event Landing Page, a dynamic and visually appealing landing page template designed using Tailwind CSS and Material Tailwind.",
+    "Website of Indie Author Oswald Corbett",
 };
 
 export default function RootLayout({
